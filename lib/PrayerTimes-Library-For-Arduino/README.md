33c42c67dd1accaea640967e3bc39ce38b357edb
Redwan Newaz <redwan06me@gmail.com>
2021-12-31 15:41:28 -0500

    azan clock added

---
+++ lib/PrayerTimes-Library-For-Arduino/README.md
@@ -0,0 +1,27 @@
+# PrayerTimes : Arduino  library class for calculating Islamic daily prayer times
+
+## Library setup
+
+* Mac users :  Create a directory named PrayerTimes inside ~/Documents/Arduino/libraries/ . Then copy the file [PrayerTimes.h](../master/src/PrayerTimes.h) into this newly created directory
+* Windows suers : Create a directory named PrayerTimes inside My Documents\Arduino\libraries\ . Then copy the file  [PrayerTimes.h](../master/src/PrayerTimes.h) into this newly created directory
+
+## Basic usage
+
+      #include <PrayerTimes.h>
+      PrayerTimes my_prayer_object;
+      double my_prayer_times[my_prayer_object.TimesCount];
+      my_prayer_object.get_prayer_times(my_year, my_month, my_day, my_latitude , my_longitude , my_timezone , my_dst_on, my_calc_method, my_asr_method, my_high_lats_method, my_prayer_times );
+
+The result will be available in the array my_prayer_times[] in the following order.
+
+      Fajr
+      Sunrise
+      Dhuhr
+      Asr
+      Sunset
+      Maghrib
+      Isha
+
+Refer to [example](example) for detailed usage of the library.
+
+Visit http://praytimes.org to find details on calculation method and code manual.
--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,41 @@
 #include "smart_clock.h"
 #include <arduino-timer.h>
 #include "wifi_manager.h"
+#include "azan_clock.h"
 
 #define RESET_PIN   (D1)
 #define MINIUTE_PIN (D2)
 #define HOUR_PIN    (D3)
 #define POWER_PIN   (D4)
 
+
 WifiManager::Manager manager; 
 
 
-const int my_timezone = -5;
+
+
 WiFiUDP ntpUDP;
 ButtonClock::Clock clock_button(POWER_PIN, RESET_PIN, HOUR_PIN, MINIUTE_PIN);
-SmartClock smart_clock(ntpUDP, "pool.ntp.org", clock_button);
+// AzanClock azan_clock(my_latitude, my_longitude, my_timezone);
+AzanClock azan_clock;
+SmartClock smart_clock(ntpUDP, "pool.ntp.org", clock_button, azan_clock);
 
-Timer<1, micros> timer;
+Timer<10, micros> timer;
 bool initialize_smart_clock(void *argument);
 bool update_smart_clock(void *argument);
+bool update_2s_clock(void *argument);
 
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(1000);
+
+  // save personal information to EEPROM 
+  // manager.write_credential("your ssid", "your password")
+  // manager.write_location("your_tz", "your_lat", "your_lon"); 
+
   Serial.print("Connecting to ");
   auto net = manager.get_credential();
   WiFi.begin(net.ssid, net.password);
@@ -37,9 +49,7 @@ void setup() {
     Serial.print(".");
   } 
 
-   // Connect to Wi-Fi
-
-  smart_clock.setTimeOffset(my_timezone * 3600);
+   // Connect to Wi-Fi  
   timer.in(5e6, initialize_smart_clock);
 }
 
@@ -52,13 +62,24 @@ void loop() {
 bool initialize_smart_clock(void *argument)
 {
   Serial.println("Resetting clock ");
+  auto loc = manager.get_location();
+  azan_clock.set_location(loc);
+
+  smart_clock.setTimeOffset(loc.timezone * 3600);
   smart_clock.reset_clock();
   timer.every(6e7, update_smart_clock);
+  // timer.every(2e6, update_2s_clock); // debuging purposes 
   return false;
 }
 
 bool update_smart_clock(void *argument)
 {
   smart_clock.update_clock();
+  return true;
+}
+
+bool update_2s_clock(void *argument)
+{
+  Serial.println("updating 2 sec clock");
   return true;
 }
