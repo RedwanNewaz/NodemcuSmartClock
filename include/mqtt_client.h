fcae244f8e923ae4846381f151331fddfab68589
RedwanNewaz <redwan06me@egmail.com>
2023-01-14 07:25:45 -0600

    mqtt reset clock feature added

--- include/mqtt_client.h
+++ include/mqtt_client.h
@@ -10,6 +10,10 @@ namespace SMART_CLOCK{
             void mqttLoop();
             void init();
         protected:
+            /// reset(0) reinitialize base::clock 
+            /// reset(1) reinitilize button::clock only
+            /// use reset(1) when internet (mqtt) time is accurate but clock display wrong time
+            /// when mqtt time is also wrong then use any number to reset the base clock and button clock
              virtual void reset(int type) = 0;
              void callback(char* topic, byte* payload, unsigned int length);
              void reconnect();
