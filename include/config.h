97945b730d88f6b76d32107ab08117193d4ec60e
RedwanNewaz <redwan06me@egmail.com>
2023-02-21 22:31:59 -0600

    url updated

--- include/config.h
+++ include/config.h
@@ -33,7 +33,8 @@ static const String PrayerNames[NUM_PRAYERS] = {"Fajr", "Dhuhr", "Asr", "Maghrib
 static const long timezone = -6; // GMT - 6
 // static const char* net = "<your ssn>";
 // static const char* password = "<your pass>";
-static const char* azanServerName  = "https://api.aladhan.com/v1/calendarByCity/2023/2?city=New%20Orleans&country=US";
+// "http://api.aladhan.com/v1/timingsByCity?city=New%20Orleans&country=US&method=2"
+static const char* azanServerName  = "http://api.aladhan.com/v1/timingsByCity?city=New%20Orleans&country=US&method=2";
 static const float soundGain = 4.0;
 
 // subscribe topic 
